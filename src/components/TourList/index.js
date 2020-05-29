87cfbcbf16a743998fbe76ec2d4f9f0f5567e61e
Natalia Sokolova <nataliya.a.sokolova@gmaol.com>
2020-05-29 12:20:47 -0700

    created components

---
+++ src/components/TourList/index.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react'
+import './TourList.scss'
+
+export default class TourList extends
+Component {
+    render() {
+        return (
+            <section>
+                tour
+            </section>
+        )
+    }
+}
