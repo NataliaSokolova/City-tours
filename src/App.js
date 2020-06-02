ee988a64311c9ecadd0baa4651cbb985dc823604
Natalia Sokolova <nataliya.a.sokolova@gmail.com>
2020-06-01 21:49:32 -0700

    added functionality for component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import TourList from './components/TourList';
 
 class  App extends Component {
   render(){
@@ -8,6 +9,7 @@ class  App extends Component {
   return (
   <main>
     <Navbar />
+    <TourList />
 
 
   </main>
